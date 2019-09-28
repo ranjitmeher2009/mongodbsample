f5a3dc54a8d4bad7929e6c8cd6296b693d881217
ranjitmeher2009 <ranjitmeher2009@gmail.com>
2019-09-28 13:09:13 +0300

    initial commit

---
+++ src/main/java/com/myproject/sample/mongodbsample/MongodbsampleApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.sample.mongodbsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbsampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongodbsampleApplication.class, args);
+    }
+
+}
